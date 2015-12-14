b1e565c65461d2dcd25eef65caa442a3afaeadf4
Mike Mikolaj  Wawrzyniak <mikolajspejs@gmail.com>
2015-12-15 00:32:38 +0100

    added some missing keywords (thanks Dominik)

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 
-var banned = ["Star Wars", "The Force Awakens","Luke Skywalker","Leia","Skywalker","Han Solo","Jedi","Sith"];
+var banned = ["Star Wars", "The Force Awakens","Luke Skywalker","Luke","Leia","Skywalker","Han Solo","Jedi","Sith","Force Awakens","Darth Vader","JJ Abrams","J.J. Abrams","J. J. Abrams","Kylo ren","rey","finn", "bb-8"];
 $.extend($.expr[":"], {
   "containsCaseInsensitive": function(elem, i, match, array) {
     return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
@@ -7,5 +7,4 @@ $.extend($.expr[":"], {
 });
 for (var i = 0; i< banned.length; i++) {
 	$("div:containsCaseInsensitive('" + banned[i] + "')").hide();
-	//$('div').find('div':containsCaseInsensitive)
 }
