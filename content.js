83dd1e041916d3d0ffed9e3325177d2dcf8c5fa3
Mike Mikolaj  Wawrzyniak <mikolajspejs@gmail.com>
2015-12-14 23:50:09 +0100

    contains is now case insensitive

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
 
 var banned = ["Star Wars", "The Force Awakens","Luke Skywalker","Leia","Skywalker","Han Solo","Jedi","Sith"];
+$.extend($.expr[":"], {
+  "containsCaseInsensitive": function(elem, i, match, array) {
+    return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+  }
+});
 for (var i = 0; i< banned.length; i++) {
-    $("div:contains('"+banned[i]+"')").each(function() {
-		 $(this).hide();
-    });
+	$("div:containsCaseInsensitive('star wars')").hide();
 }
