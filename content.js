63d947a66e0ca74786c5cff9dc8f21d832f9b880
Manuel Núñez <ianmalcom@gmail.com>
2015-12-17 14:22:15 +0100

    Black bars over sensible data
    Instead of hiding the whole page, draw black bars over Star Wars related
    data

--- content.js
+++ content.js
@@ -1,10 +1,14 @@
-
 var banned = ["Star Wars", "The Force Awakens","Luke Skywalker","Luke","Leia","Skywalker","Han Solo","Jedi","Sith","Force Awakens","Darth Vader","JJ Abrams","J.J. Abrams","J. J. Abrams","Kylo ren","rey","finn", "bb-8"];
+
 $.extend($.expr[":"], {
   "containsCaseInsensitive": function(elem, i, match, array) {
-    return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    return elem.firstChild != null && elem.firstChild.nodeValue != null && (elem.firstChild.nodeValue).toLowerCase().indexOf((match[3]).toLowerCase()) >= 0;
   }
 });
-for (var i = 0; i< banned.length; i++) {
-	$("div:containsCaseInsensitive('" + banned[i] + "')").hide();
+for (var i = 0; i < banned.length; i++) {
+	var elems = $(":containsCaseInsensitive('" + banned[i] + "')");
+	for(var j = 0; j < elems.length; j++) {
+		$(elems[j]).parent()[0].style = "position:relative";
+		$(elems[j]).append('<div style="position:absolute;top:0;left:0;width:100%;height:100%;background-color: #000;opacity: 1;z-index: 10000;"></div>');
+	}
 }
